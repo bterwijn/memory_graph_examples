5f96c7b5b6359131459a255c56d5665d82c81efa
Bas Terwijn <bterwijn@gmail.com>
2026-05-14 14:52:41 +0200

    update

--- social_media/list_simlies.py
+++ social_media/list_simlies.py
@@ -3,15 +3,16 @@ mg.config.type_to_horizontal[list] = True
 
 mylist = ['😀','😎','😍']
 
-mylist.append('😪')
-print(mylist.count('😎'))
-mylist_copy = mylist.copy()
-print(mylist.index('😍'))
-mylist.reverse()
-mylist.remove('😎')
-mylist.insert(1, '😜')
-mylist.pop()
-mylist.pop(1)
-mylist.extend(['😀','😎','😜'])
-mylist.sort()
-mylist.clear()
+print(mylist)                    # ['😀','😎','😍']
+mylist.append('😪')              # ['😀','😎','😍','😪']
+print(mylist.count('😎'))        # 1
+mylist_copy = mylist.copy()      # ['😀','😎','😍','😪']
+print(mylist.index('😍'))        # 2
+mylist.reverse()                 # ['😪','😍','😎','😀']
+mylist.remove('😎')              # ['😪','😍','😀']
+mylist.insert(1, '😜')           # ['😪','😜','😍','😀']
+mylist.pop()                     # ['😪','😜','😍']
+mylist.pop(1)                    # ['😪','😍']
+mylist.extend(['😀','😎','😜'])  # ['😪','😍','😀','😎','😜']
+mylist.sort()                    # ['😀','😍','😎','😜','😪']
+mylist.clear()                   # []
