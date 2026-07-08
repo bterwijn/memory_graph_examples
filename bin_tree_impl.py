1bf98483248a27f29e9cb771de8acbc7cf4e7fff
Bas Terwijn <bterwijn@gmail.com>
2026-07-08 23:51:29 +0200

    update

--- bin_tree_impl.py
+++ bin_tree_impl.py
@@ -19,11 +19,17 @@ class Bin_Tree_Nodes:
                         break
                     else:
                         node = node.larger
-
         if self.root is None:
             self.root = Node(data)
         else:
             _insert(data, self.root)
+
+    def get_values(self):
+        def _get_values(node):
+            if node is None:
+                return []
+            return _get_values(node.smaller) + [node.data] + _get_values(node.larger)
+        return _get_values(self.root)
 
 class Node:
     def __init__(self, data):
@@ -44,12 +50,19 @@ class Bin_Tree_List:
                     return
                 elif self.values[index] == None:
                     self.values[index] = data
+                    return
                 elif data < self.values[index]:
                     index = 2*index + 1
                 else:
                     index = 2*index + 2
-            
         _insert(0, data)
+
+    def get_values(self):
+        def _get_values(index):
+            if index >= len(self.values) or self.values[index] is None:
+                return []
+            return _get_values(2*index + 1) + [self.values[index]] + _get_values(2*index + 2)
+        return _get_values(0)
 
 bin_tree_nodes = Bin_Tree_Nodes()
 bin_tree_list = Bin_Tree_List()
@@ -58,3 +71,5 @@ for _ in range(n):
     data = random.randint(1, n*10)
     bin_tree_nodes.insert(data)
     bin_tree_list.insert(data)
+print(f'bin_tree_nodes: {bin_tree_nodes.get_values()}')
+print(f'bin_tree_list : {bin_tree_list.get_values()}')
