2f3c812ab911c65d6dc08dee2f5d470a162f54fd
Bas Terwijn <bterwijn@gmail.com>
2026-08-26 09:49:32 +0200

    update

--- lazy.py
+++ lazy.py
@@ -1,22 +1,40 @@
-
 # helper function to print and return a value
 pr = lambda s, i : print(s, i) or i
 
-def source1(N):  # eager`evalution`
+def source1(n):  # eager`evalution`
     result = []
-    for i in range(N):
+    for i in range(n):
         result.append(pr('create1:', i))
     return result
 
-def source2(N):  # lazy evaluation
-    for i in range(N):
+def source2(n):  # lazy evaluation
+    for i in range(n):
         yield pr('create2:', i)
 
-def source3(N):  # eager: list comprehension
-    return [pr('create3:', i) for i in range(N) ]
+def source3(n):  # eager: list comprehension
+    return [pr('create3:', i) for i in range(n) ]
 
-def source4(N):  # lazy: generator expression
-    return (pr('create4:', i) for i in range(N) )
+def source4(n):  # lazy: generator expression
+    return (pr('create4:', i) for i in range(n) )
+
+class MyIterator:
+    def __init__(self, current, iterable):
+        self.current = current
+        self.iterable = iterable
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current >= self.iterable.n:
+            raise StopIteration
+        value = pr('create5:', self.current)
+        self.current += 1
+        return value
+
+class Source5:  # lazy: iterable class
+    def __init__(self, n):
+        self.n = n
+    def __iter__(self):
+        return MyIterator(0, self)
 
 def sink(source):
     print('-----------------', source.__name__)
@@ -27,3 +45,5 @@ sink(source1)
 sink(source2)
 sink(source3)
 sink(source4)
+sink(Source5)
+
