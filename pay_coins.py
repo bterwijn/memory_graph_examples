aef035f10e6438504f9bf224a05f2c67bf258569
Bas Terwijn <bterwijn@gmail.com>
2026-09-17 13:32:42 +0200

    update

--- pay_coins.py
+++ pay_coins.py
@@ -7,7 +7,7 @@ def pay(amount, denominations):
         coins[coin] = count
     return amount, coins
 
-amount_to_pay = 1000 + random.randrange(1000)
+amount_to_pay = 1000 + random.randrange(2000)
 denominations = [1, 2, 5, 10, 20, 50, 100, 200]
 print(f'we have to pay: {amount_to_pay}')
 print(f'with coins: {denominations}')
@@ -16,14 +16,14 @@ print('coins:', coins)
 print('remaining amount:', remaining)
 
 coins_total = 0
-print('\nchecking the results:')
+print('\nchecking the result:')
 print(f'COIN NR_COINS COIN_TOTAL')
 for coin, count in coins.items():
     coin_total = coin * count
     print(f'{coin:4} {count:8} {coin_total:10}') 
     coins_total += coin * count
-print(f'              ---------- +')
+print(f'               --------- +')
 print(f'coins_total:  {coins_total:10}')
 print(f'remaining:    {remaining:10}')
-print(f'              ---------- +')
+print(f'               --------- +')
 print(f'              {coins_total+remaining:10}')
